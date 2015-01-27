b1c63f21e5ed68df6404904f252436576cdf08de
Braun Goodson <braungoodson@gmail.com>
2015-01-27 17:43:40 -0500

    fixed home page; added btc asset; added angular-graph; added feature graph;

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,12 @@
 
 angular.module('apolloApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
+  $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
